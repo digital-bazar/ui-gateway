bb57d8d3650ec51a4bc551299887b5e5f161f504
Murali Boyapati <murali.boyapati@epsilon.com>
2019-01-21 13:59:02 -0600

    Shell

---
+++ src/main/java/com/digital/bazar/uigateway/UiGatewayApplication.java
@@ -0,0 +1,14 @@
+package com.digital.bazar.uigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UiGatewayApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/digital/bazar/uigateway/UiGatewayApplicationTests.java
@@ -0,0 +1,17 @@
+package com.digital.bazar.uigateway;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UiGatewayApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
